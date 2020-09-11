c48dca68ce42249d40959c462b8a7ee5ace879f0
Chris Shallue <chris.shallue@gmail.com>
2020-09-11 17:15:11 +1000

    Run clang-format

--- STimer.cc
+++ STimer.cc
@@ -1,54 +1,49 @@
 #ifndef INCLUDE_STIMER
 #define INCLUDE_STIMER
 
-
 class STimer {
-public:
-    STimer();
-    ~STimer();
-    void Start(void);
-    void Stop(void);
-    double Elapsed(void);
-    void Clear(void);
+ public:
+  STimer();
+  ~STimer();
+  void Start(void);
+  void Stop(void);
+  double Elapsed(void);
+  void Clear(void);
 
-    int timeron;
+  int timeron;
 
-    struct timeval tuse, tstart, timer;
+  struct timeval tuse, tstart, timer;
 };
 
-STimer::STimer(void) { 
+STimer::STimer(void) {
+  timeron = false;
 
-    timeron = false;
-
-    timerclear(&timer);
-    timeron = 0;
+  timerclear(&timer);
+  timeron = 0;
 }
 
-STimer::~STimer() {
-}
+STimer::~STimer() {}
 
 void STimer::Start() {
-    assert(!timeron); 
-    assert( gettimeofday( &(tstart), (struct timezone *)NULL ) == 0 );
-    timeron = 1;
+  assert(!timeron);
+  assert(gettimeofday(&(tstart), (struct timezone *)NULL) == 0);
+  timeron = 1;
 }
 
 void STimer::Stop(void) {
-    assert( timeron );
-    struct timeval dt;
-    assert( gettimeofday( &(tuse), (struct timezone *)NULL ) == 0 );
-    timersub(&(tuse), &(tstart), &dt);
-    timeradd(&dt, &(timer), &(timer));
-    timeron = 0;
+  assert(timeron);
+  struct timeval dt;
+  assert(gettimeofday(&(tuse), (struct timezone *)NULL) == 0);
+  timersub(&(tuse), &(tstart), &dt);
+  timeradd(&dt, &(timer), &(timer));
+  timeron = 0;
 }
 
 void STimer::Clear(void) {
-    assert(!timeron); 
-    timerclear(&(timer));
+  assert(!timeron);
+  timerclear(&(timer));
 }
 
-double STimer::Elapsed(void) {
-    return  timer.tv_sec + 1e-6*timer.tv_usec;
-}
+double STimer::Elapsed(void) { return timer.tv_sec + 1e-6 * timer.tv_usec; }
 
-#endif // INCLUDE_STIMER
+#endif  // INCLUDE_STIMER
