d0532f562b95c117623e66932ed7cb5142427b00
Chris Shallue <chris.shallue@gmail.com>
2020-10-28 03:17:01 -0400

    Update workspace_init.fish for linux

--- workspace_init.fish
+++ workspace_init.fish
@@ -1,2 +1,10 @@
-conda_init fftcorr
-export DYLD_FALLBACK_LIBRARY_PATH=/Users/shallue/miniconda3/envs/fftcorr/lib 
+switch (uname)
+  case Linux
+    conda activate fftcorr
+    export DYLD_FALLBACK_LIBRARY_PATH=/home/cshallue/miniconda3/envs/fftcorr/lib
+  case Darwin
+    conda_init fftcorr
+    export DYLD_FALLBACK_LIBRARY_PATH=/Users/shallue/miniconda3/envs/fftcorr/lib
+  case '*'
+    echo "Unrecognized system!"
+end
