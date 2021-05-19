7df6dc1681dfda275e79ab61a02c51da7d1e0d16
Chris Shallue <chris.shallue@gmail.com>
2021-05-19 16:12:29 +1000

    Unify functions for reading halos and particles

--- fftcorr/utils/abacusutils.py
+++ fftcorr/utils/abacusutils.py
@@ -1,7 +1,8 @@
+from multiprocessing import Value
 import os.path
 import glob
 
-from abacusnbody.data.read_abacus import read_asdf
+from abacusnbody.data.bitpacked import unpack_rvint
 import asdf
 import numpy as np
 
@@ -10,16 +11,31 @@ from fftcorr.particle_mesh import MassAssignor
 from fftcorr.utils import Timer
 
 
-def read_abacus_halos(file_pattern,
-                      grid,
-                      transform_coords_fn=None,
-                      periodic_wrap=False,
-                      bounds_error="warn",
-                      verbose=True,
-                      buffer_size=10000):
+def read_density_field(file_pattern,
+                       grid,
+                       transform_coords_fn=None,
+                       periodic_wrap=False,
+                       bounds_error="warn",
+                       file_type=None,
+                       verbose=True,
+                       buffer_size=10000):
     filenames = sorted(glob.glob(file_pattern))
     if not filenames:
         raise ValueError("Found no files matching {}".format(file_pattern))
+
+    # Check the file type: halos or particles.
+    for filename in filenames:
+        basename = os.path.basename(filename)
+        if basename.startswith("halo_info"):
+            ft = "halos"
+        elif basename.startswith("field_rv"):
+            ft = "particles"
+        else:
+            raise ValueError("Unrecognized file type: '{}'".format(basename))
+        if file_type is None:
+            file_type = ft
+        if file_type != ft:
+            raise ValueError("Inconsistent file types")
 
     if bounds_error not in ["raise", "warn", "none", None]:
         raise ValueError(
@@ -28,173 +44,114 @@ def read_abacus_halos(file_pattern,
     gridmin = grid.posmin
     gridmax = grid.posmax
     box_size = None
-    total_halos = 0
-    max_halos = 0
+    total_items = 0
+    max_items = 0
 
     with Timer() as setup_timer:
-        # First, open all files and figure out the max number of halos in a
+        # First, open all files and figure out the max number of items in a
         # single file.
         for filename in filenames:
             with asdf.open(filename, lazy_load=True) as af:
                 if box_size is None:
                     box_size = af.tree["header"]["BoxSize"]
                 assert box_size == af.tree["header"]["BoxSize"]
-                n = af.tree["data"]["N"].shape[0]  # Doesn't load data.
-                total_halos += n
-                max_halos = max(max_halos, n)
+                key = "N" if file_type == "halos" else "rvint"
+                n = af.tree["data"][key].shape[0]  # Doesn't load data.
+                total_items += n
+                max_items = max(max_items, n)
         if verbose:
-            print("Found {:,} halos in {:,} files\n".format(
-                total_halos, len(filenames)))
+            print("Found {:,} items in {:,} files\n".format(
+                total_items, len(filenames)))
 
         assert box_size > 0
         if transform_coords_fn is None:
             if np.any(gridmin > -box_size / 2) or np.any(
                     gridmax < box_size / 2):
                 raise ValueError(
-                    "Grid does not cover halos: grid posmin = {}, halos posmin "
-                    "= {}, grid posmax = {}, halos posmax = {}".format(
-                        gridmin, -box_size / 2, gridmax, box_size / 2))
+                    "Grid does not cover {}: grid posmin = {}, file posmin "
+                    "= {}, grid posmax = {}, file posmax = {}".format(
+                        file_type, gridmin, -box_size / 2, gridmax,
+                        box_size / 2))
 
-        # Space for the halos in each file.
-        # TODO: the halos are actually float32s, so we could make the mass
+        # Space for the items in each file.
+        # TODO: the items are actually float32s, so we could make the mass
         # assignor accept that type without needing to cast. But we'd still need
         # to copy because asdf arrays are read only.
-        pos_buf = np.empty((max_halos, 3), dtype=np.float64, order="C")
-        weight_buf = np.empty((max_halos, ), dtype=np.float64, order="C")
+        pos_buf = np.empty((max_items, 3), dtype=np.float64, order="C")
+        weight_buf = np.empty((max_items, ), dtype=np.float64, order="C")
+        if file_type == "particles":
+            np.copyto(weight_buf, 1.0)
 
     with Timer() as work_timer:
         ma = MassAssignor(grid, periodic_wrap, buffer_size)
-        halos_seen = 0
-        halos_skipped = 0
+        items_seen = 0
+        items_skipped = 0
         io_time = 0.0
-        unit_time = 0.0
-        copy_time = 0.0
         transform_time = 0.0
         ma_time = 0.0
         for filename in filenames:
             if verbose:
                 print("Reading", os.path.basename(filename))
+
+            # Load positions and weights.
             with asdf.open(filename, lazy_load=True) as af:
-                raw_pos = af.tree["data"]["x_com"]
-                raw_weight = af.tree["data"]["N"]
-                n = raw_pos.shape[0]
-                with Timer() as io_timer:
-                    # Force the arrays to load now so we can track IO time.
-                    _ = raw_pos[0][0]
-                    _ = raw_weight[0]
-                io_time += io_timer.elapsed
-                # Copy into buffers.
+                key = "N" if file_type == "halos" else "rvint"
+                n = af.tree["data"][key].shape[0]
                 pos = pos_buf[:n]
                 weight = weight_buf[:n]
-                with Timer() as copy_timer:
-                    np.copyto(pos, raw_pos)
-                    np.copyto(weight, raw_weight)
-                copy_time += copy_timer.elapsed
-            with Timer() as unit_timer:
-                pos *= box_size
-            unit_time += unit_timer.elapsed
+                with Timer() as io_timer:
+                    if file_type == "halos":
+                        np.copyto(pos, af.tree["data"]["x_com"])
+                        np.copyto(weight, af.tree["data"]["N"])
+                        pos *= box_size
+                    else:  # file_type == "particles"
+                        npos, nvel = unpack_rvint(af.tree["data"]["rvint"],
+                                                  box_size,
+                                                  float_dtype=np.float64,
+                                                  posout=pos,
+                                                  velout=False)
+                        assert npos, nvel == (n, 0)
+                io_time += io_timer.elapsed
+
+            # Possibly transform coordinates.
             if transform_coords_fn is not None:
                 with Timer() as transform_timer:
                     transform_coords_fn(pos)
                 transform_time += transform_timer.elapsed
-            # Check if particles fall outside the grid.
+
+            # Check if any items fall outside the grid.
             if (np.any(pos.min(axis=0) < gridmin)
                     or np.any(pos.max(axis=0) >= gridmax)):
                 num_outside = np.sum(
                     np.logical_or(np.any(pos < gridmin, axis=1),
                                   np.any(pos >= gridmax, axis=1)))
-                msg = "{:,g} halos falling outside the grid".format(
-                    num_outside)
+                msg = "{:,g} {} falling outside the grid".format(
+                    num_outside, file_type)
                 if bounds_error == "raise":
                     raise ValueError(msg)
                 elif bounds_error == "warn":
                     print(msg)
                 if not periodic_wrap:
-                    halos_skipped += num_outside
+                    items_skipped += num_outside
+
+            # Add items to the density field.
             with Timer() as ma_timer:
                 ma.add_particles_to_buffer(pos, weight)
                 if filename == filenames[-1]:
                     ma.flush()  # Last file.
             ma_time += ma_timer.elapsed
-            halos_seen += n
+            items_seen += n
 
-    assert halos_seen == total_halos
-    assert ma.count + ma.skipped == halos_seen
-    assert ma.skipped == halos_skipped
+    assert items_seen == total_items
+    assert ma.count + ma.skipped == items_seen
+    assert ma.skipped == items_skipped
 
     if verbose:
         print("Setup time: {:.2f} sec".format(setup_timer.elapsed))
         print("Work time: {:.2f} sec".format(work_timer.elapsed))
         print("  IO time: {:.2f} sec".format(io_time))
-        print("  Convert units time: {:.2f} sec".format(unit_time))
-        print("  Copy time: {:.2f} sec".format(copy_time))
         if transform_coords_fn is not None:
             print("  Transform coords time: {:.2f} sec".format(transform_time))
-        print("  Mass assignor time: {:.2f} sec".format(ma_time))
-        print("    Sort time: {:.2f} sec".format(ma.sort_time))
-        print("    Window time: {:.2f} sec".format(ma.window_time))
-
-    return ma.count
-
-
-# TODO: unify with read halos function.
-def read_abacus_particles(file_pattern,
-                          grid,
-                          periodic_wrap=False,
-                          verbose=True,
-                          buffer_size=10000):
-    filenames = sorted(glob.glob(file_pattern))
-    if not filenames:
-        raise ValueError("Found no files matching {}".format(file_pattern))
-
-    with Timer() as work_timer:
-        ma = MassAssignor(grid, periodic_wrap, buffer_size)
-        particles_seen = 0
-        particles_skipped = 0
-        io_time = 0.0
-        ma_time = 0.0
-        box_size = None
-        for filename in filenames:
-            if verbose:
-                print("Reading", os.path.basename(filename))
-            with Timer() as io_timer:
-                table = read_asdf(filename,
-                                  load_pos=True,
-                                  load_vel=False,
-                                  dtype=np.float64)  # TODO: support float32
-            io_time += io_timer.elapsed
-
-            if box_size is None:
-                box_size = table.meta["BoxSize"]
-                assert box_size > 0
-                xmax = box_size / 2
-                xmin = -xmax
-            assert box_size == table.meta["BoxSize"]
-
-            if np.any(grid.posmin > xmin) or np.any(grid.posmax < xmax):
-                raise ValueError(
-                    "Grid does not cover particles: grid posmin = {}, "
-                    "particles posmin = {}, grid posmax = {}, particles "
-                    "posmax = {}".format(grid.posmin, xmin, grid.posmax, xmax))
-
-            pos = table["pos"].data
-            if not periodic_wrap and (pos.min() < xmin or pos.max() >= xmax):
-                num_skip = np.sum(np.logical_or(pos < xmin, pos >= xmax))
-                if verbose:
-                    print("{:,g} particles falling outside the grid will be "
-                          "skipped".format(num_skip))
-                particles_skipped += num_skip
-            with Timer() as ma_timer:
-                ma.add_particles(pos, weight=1.0)
-            ma_time += ma_timer.elapsed
-            particles_seen += pos.shape[0]
-
-    assert ma.count + ma.skipped == particles_seen
-    assert ma.skipped == particles_skipped
-
-    if verbose:
-        print("Work time: {:.2f} sec".format(work_timer.elapsed))
-        print("  IO time: {:.2f} sec".format(io_time))
         print("  Mass assignor time: {:.2f} sec".format(ma_time))
         print("    Sort time: {:.2f} sec".format(ma.sort_time))
         print("    Window time: {:.2f} sec".format(ma.window_time))
