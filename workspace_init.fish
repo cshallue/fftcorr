40efeb6603b2906aa9eb9b834807ae3bfc748091
Chris Shallue <chris.shallue@gmail.com>
2021-06-17 16:51:56 +1000

    Set PYTHONPATH in workspace_init.fish

--- workspace_init.fish
+++ workspace_init.fish
@@ -1,10 +1,11 @@
 switch (uname)
-  case Linux
-    conda activate fftcorr
-    export DYLD_FALLBACK_LIBRARY_PATH=/home/cshallue/miniconda3/envs/fftcorr/lib
-  case Darwin
-    conda_init fftcorr
-    export DYLD_FALLBACK_LIBRARY_PATH=/Users/shallue/miniconda3/envs/fftcorr/lib
-  case '*'
-    echo "Unrecognized system!"
+    case Linux
+        conda activate fftcorr
+        export DYLD_FALLBACK_LIBRARY_PATH=/home/cshallue/miniconda3/envs/fftcorr/lib
+    case Darwin
+        conda_init fftcorr
+        export DYLD_FALLBACK_LIBRARY_PATH=/Users/shallue/miniconda3/envs/fftcorr/lib
+    case '*'
+        echo "Unrecognized system!"
 end
+set --export "PYTHONPATH $HOME/git/fftcorr:$PYTHONPATH"
